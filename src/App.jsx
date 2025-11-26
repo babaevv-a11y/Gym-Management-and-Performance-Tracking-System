c94ccec5178783d76ffbbb31e9296d1f807ec319
babaevv-a11y <babaevv@vcu.edu>
2025-11-26 11:21:33 -0500

    Update App.jsx to include all PBIs and render PbiA

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import PbiC from './features/pbiC/PbiC';
 import PbiD from './features/pbiD/pbiD';
+import PbiA from './features/pbiA/pbiA';
 
 function App() { // If I want to show pbiC instead of pbiD then I can simply replace <pbiD /> with the <pbiC />
   return (
     <div>
-      <PbiD />
+      <pbiA />
 
     </div>
   );
